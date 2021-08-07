7b4afa76fe3aea1b0505abfcae478dcdeddfd6b8
RiffSphere <58853004+RiffSphere@users.noreply.github.com>
2021-08-07 20:40:01 +0200

    Update collectarr.py

--- collectarr.py
+++ collectarr.py
@@ -425,7 +425,8 @@ start_time = datetime.datetime.now().strftime("%y-%m-%d_%H-%M-%S")
 # Config file and folder Check
 #     Allow config file to be passed as parameter, useful for docker
 if len(sys.argv) != 1 and sys.argv[1][0] != "-":
-   config_path = sys.argv[1] + "/"
+   config_path = sys.argv[1] 
+   if not config_path[-1:]=="/": config_path=config_path+"/"
 #     If not passed, use current dir
 else:
    config_path = "./"
