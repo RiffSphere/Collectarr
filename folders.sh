09e7334b8b2c952f8f494ac892090844bc427905
RiffSphere <58853004+RiffSphere@users.noreply.github.com>
2021-08-08 11:58:30 +0200

    Update folders.sh

--- folders.sh
+++ folders.sh
@@ -3,6 +3,12 @@ if [[ ! -f /config/collectarr.conf ]]; then
     echo "First run, cloning config into /config"
     mv /app/collectarr.conf.example /config
 fi
+if [[ ! -f /config/blacklist_collection.conf ]]; then
+    mv /app/blacklist_collection.conf /config
+fi
+if [[ ! -f /config/blacklist_actor.conf ]]; then
+    mv /app/blacklist_actor.conf /config
+fi
 
 chown -R root:root /config
 chmod -R 777 /config
